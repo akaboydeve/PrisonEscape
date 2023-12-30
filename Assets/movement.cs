83c9a22edf4023d4ba7aba4e952243165ee88db1
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-12-30 16:03:29 +0530

    files

---
+++ Assets/movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 5f;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb2d.velocity = new Vector2(vertical * speed , rb2d.velocity.y);
+        
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.x);
+
+
+    }
+}
